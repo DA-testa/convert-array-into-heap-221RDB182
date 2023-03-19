5fcbd63a6a7a2ba9fce34b203008f0aa70029e19
Aleksandrs K <123946459+221RDB182@users.noreply.github.com>
2023-03-19 16:30:19 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -2,15 +2,13 @@ def build_heap(data):
     swaps = []
     n = len(data)
     
-    for i in range(n//2, -1, -1):
-        shift_down(data, swaps, i)
-    
-   def shift_down(data, swaps, i):
+    def shift_down(data, swaps, i):
+        nonlocal n
         min_index = i
         left = 2*i+1
         right = 2*i+2
 
-    if left < n and data[left] < data[min_index]:
+        if left < n and data[left] < data[min_index]:
             min_index = left
 
         if right < n and data[right] < data[min_index]:
@@ -20,6 +18,9 @@ def build_heap(data):
             swaps.append((i, min_index))
             data[i], data[min_index] = data[min_index], data[i]
             shift_down(data, swaps, min_index)
+
+    for i in range(n//2, -1, -1):
+        shift_down(data, swaps, i)
 
     return swaps
 
