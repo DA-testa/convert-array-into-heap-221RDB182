b9a4c23ea4c112a78c83f7cd7eb6bb06b779db1a
Aleksandrs K <123946459+221RDB182@users.noreply.github.com>
2023-03-19 16:47:54 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,12 +1,15 @@
 def build_heap(data):
     swaps = []
     n = len(data)
-    
-    def shift_down(data, swaps, i):
-        nonlocal n
+
+    for i in range(n // 2, -1, -1):
+        shift_down(i)
+
+    def shift_down(i):
+        nonlocal swaps
         min_index = i
-        left = 2*i+1
-        right = 2*i+2
+        left = 2 * i + 1
+        right = 2 * i + 2
 
         if left < n and data[left] < data[min_index]:
             min_index = left
@@ -17,43 +20,30 @@ def build_heap(data):
         if i != min_index:
             swaps.append((i, min_index))
             data[i], data[min_index] = data[min_index], data[i]
-            shift_down(data, swaps, min_index)
-
-    for i in range(n//2, -1, -1):
-        shift_down(data, swaps, i)
+            shift_down(min_index)
 
     return swaps
 
 def main():
-    # input from keyboard
-    text = input("Enter 'I' for input, 'F' for file...")
-    
-   if "F" in text:
-        file_name = input("Enter file name: ")
-        if "a" not in file_name:
-            path = './tests/' + file_name
-            with open(path, 'r', encoding='utf-8') as file:
-                n = int(file.readline())
-                data = list(map(int, file.readline().split()))
-    elif "I" in text:
-        n = int(input())
-        data = list(map(int, input().split()))
-              
-    # checks if lenght of data is the same as the said lenght
-    assert data is not None and len(data) == n
+    while True:
+        text = input("Enter I for input, F for file...")
 
-    # calls function to assess the data 
-    # and give back all swaps
+        if "I" in text:
+            n = int(input("Enter size of array..."))
+            data = list(map(int, input("Enter elements of array...").split()))
+            break
+
+        if "F" in text:
+            file_name = input("Enter file name...")
+            with open(file_name, "r") as f:
+                n = int(f.readline())
+                data = list(map(int, f.readline().split()))
+            break
+
     swaps = build_heap(data)
-    
-    # this number should be less than 4n (less than 4*len(data))
-    assert len(swaps) <= n*4
-
-    # output all swaps
     print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
+    for swap in swaps:
+        print(swap[0], swap[1])
 
 if __name__ == "__main__":
     main()
