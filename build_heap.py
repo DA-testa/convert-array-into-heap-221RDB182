053431c5017c290d960b93701f968101dd9820c0
Aleksandrs K <123946459+221RDB182@users.noreply.github.com>
2023-03-19 16:24:10 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -5,42 +5,44 @@ def build_heap(data):
     for i in range(n//2, -1, -1):
         shift_down(i)
 
-    def shift_down(i):
-        nonlocal swaps
-        min_index = i
-        left = 2*i+1
-        right = 2*i+2
+    def shift_down(data, swaps, i):
+    n = len(data)
+    min_index = i
+    left = 2*i+1
+    right = 2*i+2
 
-        if left < n and data[left] < data[min_index]:
-            min_index = left
+    if left < n and data[left] < data[min_index]:
+        min_index = left
 
-        if right < n and data[right] < data[min_index]:
-            min_index = right
+    if right < n and data[right] < data[min_index]:
+        min_index = right
 
-        if i != min_index:
-            swaps.append((i, min_index))
-            data[i], data[min_index] = data[min_index], data[i]
-            shift_down(min_index)
+    if i != min_index:
+        swaps.append((i, min_index))
+        data[i], data[min_index] = data[min_index], data[i]
+        shift_down(data, swaps, min_index)
+        
+    for i in range(n//2, -1, -1):
+        shift_down(data, swaps, i)
     return swaps
 
 
 def main():
     # input from keyboard
     text = input("Enter I for input, F for file...")
-    if "F" in text:
+    if "I" in text:
+        n = int(input("Enter size of array..."))
+        data = list(map(int, input("Enter elements of array...").split()))
+    else:
+        print("Error. Please enter 'I' or 'F'.")
+        
+    elif "F" in text:
         file_name = input("Enter file name: ")
         if "a" not in file_name:
             path = './tests/' + file_name
             with open(path, 'r', encoding='utf-8') as file:
                 n = int(file.readline())
                 data = list(map(int, file.readline().split()))
-    
-        
-    elif "I" in text:
-        n = int(input("Enter size of array..."))
-        data = list(map(int, input("Enter elements of array...").split()))
-    else:
-        print("Error. Please enter 'I' or 'F'.")
  
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
