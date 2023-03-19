c45fd62eb43c3c2ec7fb3af46afce197badb213d
Aleksandrs K <123946459+221RDB182@users.noreply.github.com>
2023-03-19 17:39:16 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -9,23 +9,25 @@ def build_heap(data):
 
         if left < size and data[left] < data[min_index]:
             min_index = left
+            
         if right < size and data[right] < data[min_index]:
             min_index = right
+            
         if i != min_index:
             data[i], data[min_index] = data[min_index], data[i]
             swaps.append((i, min_index))
             shift_down(min_index)
             
-        def shift_down(i):
+    def shift_down(i):
         change(i)
 
-        for i in range(size // 2, -1, -1):
+    for i in range(size // 2, -1, -1):
         shift_down(i)
 
     return swaps
 
 
-def main():
+def main(): # input from keyboard
     text = input("Enter 'I' (input) or 'F' (file)")
     if "F" in text:
         file_name = input("file name: ")
@@ -36,12 +38,12 @@ def main():
     elif "I" in text:
         size = int(input())
         data = list(map(int, input().split()))
+                                         
+    assert data is not None and len(data) == size   # checks if lenght of data is the same as the said lenght
+    swaps = build_heap(data)                        # calls function to assess the data and give back all swaps
+    assert len(swaps) <= size*4                     # this number should be less than 4n (less than 4*len(data))
 
-    assert data is not None and len(data) == size
-    swaps = build_heap(data)
-    assert len(swaps) <= size*4
-
-    print(len(swaps))
+    print(len(swaps))   # output all swaps
     for i, j in swaps:
         print(i, j)
 
